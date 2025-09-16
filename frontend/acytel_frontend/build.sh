5b225e5e59c378b086ce997d7cfbd94b81eb5b43
Harsh Rawat <technicalcode578@gmail.com>
2025-09-16 11:31:02 +0000

    fix(frontend): run cargo fix to remove warnings

--- frontend/acytel_frontend/build.sh
+++ frontend/acytel_frontend/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
-cd src/audio-engine && wasm-pack build --target web && cd ../.. && vite build
+cd src/audio-engine
+cargo fix --lib -p audio-engine
+wasm-pack build --target web
+cd ../..
+vite build
