a93885612da93554a002cc750579e7fd2d0a0c5c
Harsh Rawat <technicalcode578@gmail.com>
2025-09-16 11:34:06 +0000

    fix(frontend): allow dirty cargo fix

--- frontend/acytel_frontend/build.sh
+++ frontend/acytel_frontend/build.sh
@@ -2,7 +2,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 cd src/audio-engine
-cargo fix --lib -p audio-engine
+cargo fix --lib -p audio-engine --allow-dirty
 wasm-pack build --target web
 cd ../..
 vite build
